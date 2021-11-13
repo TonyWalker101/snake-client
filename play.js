9241bfea55fc21e879afac5385bcbae94511095d
Tony <awalker780@gmail.com>
2021-11-13 18:10:26 -0500

    Created initial code that connects to server

---
+++ play.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+// sets up connetion with game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+}
+
+console.log("Connecting ...");
+connect();
