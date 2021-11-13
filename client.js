f113f8cfc350f6e007920c212f3f185a5bb18e1a
Tony <awalker780@gmail.com>
2021-11-13 18:45:08 -0500

    Exported function to module

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+// sets up connetion with game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+};
+
+module.exports = {
+  connect,
+}
